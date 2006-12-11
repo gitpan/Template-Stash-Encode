7798180850a9c78c2184eb91775fc2bae16a1a08
Toru Yamaguchi <trymch+public@gmail.com>
2006-12-11 17:20:48 +0000

    Import of ZIGOROU/Template-Stash-Encode-0.03 from CPAN.
    gitpan-cpan-distribution: Template-Stash-Encode
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         ZIGOROU/Template-Stash-Encode-0.03.tar.gz
    gitpan-cpan-author:       ZIGOROU
    gitpan-cpan-maturity:     released

--- lib/Template/Stash/Encode.pm
+++ lib/Template/Stash/Encode.pm
@@ -14,11 +14,11 @@ Template::Stash::Encode - Encode charactor code on stash variables
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
